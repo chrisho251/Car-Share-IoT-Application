88ccb68649158ff48a3d23dfa6005d0f2c7fbc31
chrisho251 <hoduyhoang250196@gmail.com>
2020-09-21 16:31:05 +0700

    Update flask_site.py

--- src/flask_site.py
+++ src/flask_site.py
@@ -1,25 +1,19 @@
 from flask import Flask, flash, Blueprint, request, jsonify, render_template, redirect, url_for, session, json, jsonify, make_response, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
-import requests
-import json
-import sys
-import re
-import datetime
+import os, requests, json, sys, re, datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from user_api import User
-from schema import UserSchema
-from car_api import Car
-from schema import carSchema
-from schema import carsSchema
+from user_api import *
+from schema import *
+from car_api import *
+from schema import *
+from schema import *
 import pathlib
 sys.path.append(os.path.abspath('../Facial recognition'))
 sys.path.append(os.path.abspath('../../src/QRReader'))
 from create_qr import create_qr
 from PIL import Image
-import glob
-import shutil
+import glob, shutil
 
 app = Flask(__name__)
 site = Blueprint("site", __name__)
@@ -33,12 +27,12 @@ def login():
 
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password'] 
 
         # api call to save the user.
         response = requests.post(
-            "http://localhost:8080/api/login", {"email": username, "password": password})
+            "http://localhost:8080/api/login", {"email": email, "password": password})
         #print(response)
         data = json.loads(response.text)
 
@@ -47,7 +41,7 @@ def login():
             # Create session data, we can access this data in other routes
             session['logged_in'] = True
             session['user_id'] = data['user_id']
-            session['username'] = data['email']
+            session['email'] = data['email']
             session['user_class'] = data['user_class']
             session['mac_address'] = data['mac_address']
 
@@ -55,7 +49,7 @@ def login():
             return redirect(url_for('site.home'))
         else:
             # Account doesnt exist or username/password incorrect
-            msg = 'Wrong username/password.'
+            msg = 'Wrong username or password. Please check again'
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
 
@@ -96,9 +90,9 @@ def home():
 
                 bookings_response = requests.get("http://localhost:8080/api/bookings")
                 bookings = json.loads(bookings_response.text)
-                return render_template('admin/admin-dashboard.html', username=session['username'], cars=cars, users=users, bookings=bookings)
+                return render_template('admin/admin-dashboard.html', username=session['email'], cars=cars, users=users, bookings=bookings)
             else:
-                return render_template('home.html', username=session['username'], cars=cars)
+                return render_template('home.html', username=session['email'], cars=cars)
     # users is not loggedin redirect to login page
     return redirect(url_for('site.login'))
 
@@ -266,7 +260,7 @@ def edit_user():
 
 
 @site.route('/deluser', methods=['POST'])
-def del_user():
+def delete_user():
 
     if 'logged_in' in session:
         user_id = request.form['user_id']
